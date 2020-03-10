02235f0c983fdd7ac25ebcc26e88255fde782901
wyjsusan <yujuewa@gmail.com>
2020-03-09 21:10:38 -0700

    init with sql data.

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+insert into user (name, user_name, password, gender, phone_number, address, register_date, is_manager, is_deleted) values ('张三', 'user','123456','0','13009129000', '309号路20街', '20200309', '0', '0');
+
+insert into user (name, user_name, password, gender, phone_number, address, register_date, is_manager, is_deleted) values ('李四', 'admin','123456','1','13009129000', '历史号路20街', '20200309', '1', '0');
+
+insert into activity(name, content, start_time, end_time, enroll_state, publish_time, is_deleted, is_completed) gitvalues ('测试活动','测试活动内容', '2020-03-09', '2020-03-29', '1', '20200309', '0', '0');
