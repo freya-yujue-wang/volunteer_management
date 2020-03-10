0d96bce9d7fc83a6955feec51e57599233d7c376
wyjsusan <yujuewa@gmail.com>
2020-03-09 21:25:48 -0700

    fix bug.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,4 +2,4 @@ insert into user (name, user_name, password, gender, phone_number, address, regi
 
 insert into user (name, user_name, password, gender, phone_number, address, register_date, is_manager, is_deleted) values ('李四', 'admin','123456','1','13009129000', '历史号路20街', '20200309', '1', '0');
 
-insert into activity(name, content, start_time, end_time, enroll_state, publish_time, is_deleted, is_completed) gitvalues ('测试活动','测试活动内容', '2020-03-09', '2020-03-29', '1', '20200309', '0', '0');
+insert into activity(name, content, start_time, end_time, enroll_state, publish_time, is_deleted, is_completed) values ('测试活动','测试活动内容', '2020-03-09', '2020-03-29', '1', '20200309', '0', '0');
